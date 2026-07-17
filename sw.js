82230c902ea7e66aeab03e7a53052f1f35d2caa9
humedalcostero-blip <humedalcostero@gmail.com>
2026-07-17 11:10:46 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE="cuenta-v1";
-const FILES=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
-});
-self.addEventListener("fetch",event=>{
-  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
-});
